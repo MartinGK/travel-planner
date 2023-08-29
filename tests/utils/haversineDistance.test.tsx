08d46042061d3bfc0c61ec8f4fd637be8fde1f26
MartinGK <mrtgeka@gmail.com>
2023-08-29 10:46:47 -0300

    feat: tests added to haversine formula

--- tests/utils/haversineDistance.test.tsx
+++ tests/utils/haversineDistance.test.tsx
@@ -1,16 +1,33 @@
 import haversineDistance, { Points } from "../../src/utils/haversineDistance";
 import AppendixA from "../../src/utils/AppendixA.json";
 
-const [_, ...ParisPoints] = AppendixA[0];
-const [__, ...MarseillePoints] = AppendixA[1];
+const [, ...ParisPoints] = AppendixA[0];
+const [, ...MarseillePoints] = AppendixA[1];
+const [, ...LyonPoints] = AppendixA[2];
+const [, ...DijonPoints] = AppendixA.filter((data) => data[0] === "Dijon")[0];
 
-describe("haversineDistance", () => {
-  test("calculates haversine distance correctly", () => {
+describe("haversineDistance: calculates haversine distance between", () => {
+  test("Paris & Marseille", () => {
     const distance = haversineDistance({
       firstPoints: ParisPoints as Points,
       secondPoints: MarseillePoints as Points,
     });
-    // This is a rough estimate, not an exact value
     expect(distance).toBeCloseTo(660.5, 1); // Rounded to 1 decimal
+  });
+
+  test("Marseille & Lyon", () => {
+    const distance = haversineDistance({
+      firstPoints: MarseillePoints as Points,
+      secondPoints: LyonPoints as Points,
+    });
+    expect(distance).toBeCloseTo(277.6, 1); // Rounded to 1 decimal
+  });
+
+  test("Paris & Dijon", () => {
+    const distance = haversineDistance({
+      firstPoints: ParisPoints as Points,
+      secondPoints: DijonPoints as Points,
+    });
+    expect(distance).toBeCloseTo(262.7, 1); // Rounded to 1 decimal
   });
 });
