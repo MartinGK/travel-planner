c58a12dd9ad99a7ae6048b5b30b400be2a1fb0a8
MartinGK <mrtgeka@gmail.com>
2023-09-02 13:58:24 -0300

    feat: endpoints modified

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,31 +1,54 @@
 import haversineDistance from "./haversineDistance";
+const ERROR_STRING = "fail";
+const ERROR_CITY = "Dijon";
 
-type DataResponse = {
+export type DataResponse = {
   cities: Cities;
 };
 
-type Cities = City[];
-type City = [string, number, number];
-type CitiesDistances = { [k: string]: number };
+export type Cities = City[];
+export type City = [string, number, number];
+export type CitiesDistances = { [k: string]: number };
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const getAllCities = async (): Promise<Cities> => {
-  const res: Response = await fetch("http://localhost:3001/cities");
-  const data: DataResponse = await res.json();
+export const getAllCitiesData = async (): Promise<Cities> => {
   await sleep(3000);
-  return data.cities;
+  return fetch("http://localhost:3001/cities").then((res) => res.json());
+};
+
+export const getAllCitiesNames = async (): Promise<string[]> => {
+  const allCities = await getAllCitiesData();
+  return allCities.map(([cityName]) => cityName);
 };
 
 //The first endpoint receives a keyword and returns a list of cities that match the keyword.
 export const getRecommendationsList = async (keyword: string) => {
-  const allCities = await getAllCities();
-  return allCities.filter((city) => city[0].includes(keyword));
+  if (keyword.toLowerCase() === ERROR_STRING.toLowerCase()) {
+    throw new Error(`Oops! I did it again!`);
+  }
+  const allCitiesNames = await getAllCitiesNames();
+
+  const recommendations = allCitiesNames.filter((c) =>
+    c.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  while (recommendations.length <= 2) {
+    const index = Math.floor(Math.random() * allCitiesNames.length);
+    if (!recommendations.includes(allCitiesNames[index])) {
+      recommendations.push(allCitiesNames[index]);
+    }
+  }
+
+  while (recommendations.length > 3) {
+    recommendations.pop();
+  }
+  return recommendations;
 };
 
 //The second endpoint receives a list of cities and calculates the distances.
 export const calculatesCitiesDistances = async (citiesList: Array<string>) => {
-  const allCities = await getAllCities();
+  const allCities = await getAllCitiesData();
   const res: CitiesDistances = {};
   for (let i = 0; i < citiesList.length - 1; i++) {
     const [firstCityName, ...firstPoints] = findCity(allCities, citiesList[i]);
@@ -40,6 +63,9 @@ export const calculatesCitiesDistances = async (citiesList: Array<string>) => {
 };
 
 const findCity = (allCities: Cities, cityNameToFind: string): City => {
+  if (cityNameToFind === ERROR_CITY) {
+    throw new Error(`John, you can't travel to Dijon.`);
+  }
   const foundCity = allCities.find(([cityName]) => cityName === cityNameToFind);
   if (foundCity) return foundCity;
   throw new Error(`Could not find ${cityNameToFind}`);
