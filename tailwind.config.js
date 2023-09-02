dc1f0f05fa294c12f04eed1f18070748b974a9c2
MartinGK <mrtgeka@gmail.com>
2023-09-02 17:07:31 -0300

    feat: configs modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  theme: {
+    colors: {
+      ...colors,
+      transparent: "transparent",
+      current: "currentColor",
+      purple: "#7786D2",
+      "light-purple": "#C7D1F4",
+    },
+  },
 };
